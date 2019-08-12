4573562ba5a29c831c7fd17407dc81400e5e015e
zhuxingxing <zhuxingxing@zhuxingxing.local>
2019-08-12 16:50:05 +0800

    feat(): express

--- 17.koa-middleware/1.server.js
+++ 17.koa-middleware/1.server.js
@@ -8,16 +8,19 @@ router.get('/hello', async (ctx, next) => {
     ctx.body = 'hello';
     next();
 })
+
 router.get('/hello', async (ctx, next) => {
     ctx.body = 'hello2';
     next();
 })
+
 router.get('/world', async (ctx, next) => {
     ctx.body = 'world';
     next(); // 这个next相当于app.use(router.routes())中的next,会执行下一个use方法
 })
+
 // 装载路由
-app.use(router.routes()); // 使用中渐渐的方法,挂载到app上   原生提供了一个routes方法,相当于componse
+app.use(router.routes()); // 使用中间件的方法,挂载到app上   原生提供了一个routes方法,相当于componse
 app.use(async ctx => {
     ctx.body = '你好' // 17行的next执行,会覆盖之前的hello/world
 })
